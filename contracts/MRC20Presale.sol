1b701032215205513a7af7ca552a19230202c1e5
Reza Bakhshandeh <reza.bakhshandeh@gmail.com>
2021-12-30 14:04:31 +0330

    code cleanup + supporting burnable tokens

--- contracts/MRC20Presale.sol
+++ contracts/MRC20Presale.sol
@@ -2,15 +2,9 @@
 pragma solidity ^0.8.0;
 
 import './IMuonV02.sol';
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import './IMRC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
-
-interface IMRC20 is IERC20 {
-  function decimals() external returns (uint8);
-  function mint(address reveiver, uint256 amount) external returns (bool);
-  function burn(address sender, uint256 amount) external returns (bool);
-}
 
 contract MRC20Presale is Ownable {
   using ECDSA for bytes32;
